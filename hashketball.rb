9709be0dba1e74c1182072529689784e7e1337d1
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2020-10-23 08:16:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,5 @@
 # Write your code below game_hash
+require 'pry'
 def game_hash
   {
     home: {
@@ -127,3 +128,7 @@ def game_hash
 end
 
 # Write code here
+def num_points_scored (name)
+  index = game_hash[:home][:players].index(name)
+  binding.pry
+end
